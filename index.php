df87ca78e2780b67e87a62e38c969ab6402a50f2
Emiliano Ostellino <73698038+emioj89@users.noreply.github.com>
2021-12-02 09:34:09 +0100

    Update index.php

--- index.php
+++ index.php
@@ -3,12 +3,12 @@
 $presentTime = new DateTime;
 $destinationTime = new DateTime;
 
-$destinationTime->setDate(2019, 8, 16)->setTime(01, 30);
+$destinationTime->setDate(2021, 10, 04)->setTime(09, 30);
 
 echo 'Present time : ' . $presentTime->format('M d Y A h : i') . "</br>";
 echo 'Destination time : ' . $destinationTime->format('M d Y A h : i') . "</br>";
 
 $timeUntilDestination = $presentTime->diff($destinationTime);
-echo $timeUntilDestination->format('Mon fils est né il y a %Y ans %M mois %d jours %h heures et %i minutes');
+echo $timeUntilDestination->format('Ma fille est né il y a %Y ans %M mois %d jours %h heures et %i minutes');
 
 ?>
